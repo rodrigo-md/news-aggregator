1b657a5dcbfa5d28e48996d3c00fd8a0dc224a72
Rodrigo Martinez Diaz <martinez.d.rodrigo@gmail.com>
2025-07-15 04:08:10 -0400

    chore: setup terraform backend

---
+++ infra/variables.tf
@@ -0,0 +1,10 @@
+variable "project_name" {
+  description = "The name of the project"
+  type        = string
+}
+
+variable "environment" {
+  description = "The environment for the project (e.g., dev, staging, prod)"
+  type        = string
+}
+
